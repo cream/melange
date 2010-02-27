ee53f974eb16c0497876f68c226ef1c94f7ebf11
Immanuel Peratoner <immanuel@think-factory.(none)>
2010-02-27 15:24:14 +0100

    * Polished melange.css Style * Improved layout of Paste Widget, Example Widget and Calculator widget * Added new clipboard icon to Paste Widget * Added new calculator icon * Fixed bugs in Calculator Widget and improved calculator display

--- widgets/Calculator/skins/default/calculator.js
+++ widgets/Calculator/skins/default/calculator.js
@@ -1,28 +1,53 @@
-var lastcommand = ''
-var lastnumber = ''
-var actnumber = ''
+var lastcommand = '';
+var lastnumber = '';
+var actnumber = '';
+var commands = new Array();
+commands["+"] = "+";
+commands["-"] = "-";
+commands["&#247;"] = "/";
+commands["&#215;"] = "*";
+commands["&radic;"] = "SQRT";
 
 function addnumber(num){
-	actnumber = actnumber + num
-	document.getElementById('lcd').innerHTML = actnumber
+	actnumber = actnumber + num;
+	if (document.getElementById('lcd').innerHTML == '0') {
+		document.getElementById('lcd').innerHTML = actnumber;
+	}
+	else {
+		document.getElementById('lcd').innerHTML += num;
+	}
 }
 
 function reset(){
-	lastcommand = ''
-	lastnumber = ''
-	actnumber = ''
-	document.getElementById('lcd').innerHTML = '0'
+	lastcommand = '';
+	lastnumber = '';
+	actnumber = '';
+	document.getElementById('lcd').innerHTML = '0';
 }
 
-function command(command){
-	lastcommand = command
-	lastnumber = actnumber
-	actnumber = ''
-	document.getElementById('lcd').innerHTML = '0'
+function command(command){    
+	// calculate the current
+	lastnumber = calc(false);
+	lastcommand = command;
+	actnumber = '';
+    if (command != 'SQRT') {
+    	document.getElementById('lcd').innerHTML += command;
+    }
+    else {
+        document.getElementById('lcd').innerHTML = '√' + document.getElementById('lcd').innerHTML;
+    }
 }
 
-function calc(){
-	sum = eval(lastnumber + lastcommand + actnumber)
-	document.getElementById('lcd').innerHTML = sum
-	actnumber = sum
+function calc(display){
+    if (lastcommand != 'SQRT') {
+    	sum = eval(lastnumber + lastcommand + actnumber);
+    }
+    else {
+        sum = Math.sqrt(lastnumber);
+    }
+    if (display == true) {
+    	document.getElementById('lcd').innerHTML = sum;
+    }
+	actnumber = sum;
+	return sum;
 }
