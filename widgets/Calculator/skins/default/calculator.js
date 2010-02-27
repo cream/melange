09735c7500c7d4bbe2cf561d762af76124627dd9
Immanuel Peratoner <immanuel.peratoner@gmail.com>
2010-02-28 00:37:50 +0100

    Calculator widget cleanup and improvements.
    * Cleaned up code of the calculator widget
    * Added support for adding more complex mathematical operations to the calculator widget
    * Improved layout/style of the calculator widget

--- widgets/Calculator/skins/default/calculator.js
+++ widgets/Calculator/skins/default/calculator.js
@@ -1,53 +1,84 @@
-var lastcommand = '';
-var lastnumber = '';
-var actnumber = '';
-var commands = new Array();
-commands["+"] = "+";
-commands["-"] = "-";
-commands["&#247;"] = "/";
-commands["&#215;"] = "*";
-commands["&radic;"] = "SQRT";
+var BEFORE = 'before';
+var AFTER = 'after';
+var TYPE_DEFAULT = 'default-operator';
+var TYPE_SPECIAL = 'special-operator';
 
-function addnumber(num){
-	actnumber = actnumber + num;
+var last_command = '';
+var last_command_type = TYPE_DEFAULT;
+var last_number = '';
+var act_number = '';
+
+var special_commands = new Array();
+special_commands['SQRT'] = Array('√', 'Math.sqrt(%i)', BEFORE);
+special_commands['EXP'] = Array('^', 'Math.pow(%i, %ii)', AFTER);
+
+var commands = new Array();
+commands['+'] = '+';
+commands['-'] = '−';
+commands['*'] = '×';
+commands['/'] = '÷';
+
+function round_float(num, n) {
+	num = (Math.round(num * n) / n);
+   	return num;
+}
+
+function add_number(num) {
+	act_number = act_number + num;
+
 	if (document.getElementById('lcd').innerHTML == '0') {
-		document.getElementById('lcd').innerHTML = actnumber;
+		document.getElementById('lcd').innerHTML = act_number;
 	}
 	else {
 		document.getElementById('lcd').innerHTML += num;
 	}
 }
 
-function reset(){
-	lastcommand = '';
-	lastnumber = '';
-	actnumber = '';
+function reset_display() {
+	last_command = '';
+    last_command_type = TYPE_DEFAULT;
+	last_number = '';
+	act_number = '';
 	document.getElementById('lcd').innerHTML = '0';
 }
 
-function command(command){    
-	// calculate the current
-	lastnumber = calc(false);
-	lastcommand = command;
-	actnumber = '';
-    if (command != 'SQRT') {
-    	document.getElementById('lcd').innerHTML += command;
+function set_command(command, special) {
+	last_number = calc(false);
+	last_command = command;
+	act_number = '';
+    if (special == true) {
+        last_command_type = TYPE_SPECIAL;
+        if (special_commands[command][2] == BEFORE) {
+        	document.getElementById('lcd').innerHTML = special_commands[command][0] + document.getElementById('lcd').innerHTML;
+        }
+        else if (special_commands[command][2] == AFTER) {
+        	document.getElementById('lcd').innerHTML += special_commands[command][0];
+        }
     }
     else {
-        document.getElementById('lcd').innerHTML = '√' + document.getElementById('lcd').innerHTML;
+        last_command_type = TYPE_DEFAULT;
+    	document.getElementById('lcd').innerHTML += commands[command];
     }
 }
 
-function calc(display){
-    if (lastcommand != 'SQRT') {
-    	sum = eval(lastnumber + lastcommand + actnumber);
+function calc(display) {
+    if (last_command_type == TYPE_DEFAULT) {
+    	sum = round_float(eval(last_number + last_command + act_number), 1000000);
     }
-    else {
-        sum = Math.sqrt(lastnumber);
+    else if (last_command_type == TYPE_SPECIAL) {
+        com = special_commands[last_command][1].replace('%i', last_number);
+        com = com.replace('%ii', act_number);
+        sum = round_float(eval(com), 1000000);
     }
+
     if (display == true) {
     	document.getElementById('lcd').innerHTML = sum;
     }
-	actnumber = sum;
+    
+    act_number = '';
+	last_number = sum;
+    last_command = '';
+    last_command_type = TYPE_DEFAULT;
+
 	return sum;
 }
