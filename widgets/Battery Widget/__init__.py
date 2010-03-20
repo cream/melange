6b44b3199067ae6bcf3452269811ede124d00d75
Immanuel Peratoner <immanuel.peratoner@gmail.com>
2010-03-20 23:23:33 +0100

    Finally adding mootools-more; Added tab-option to chooser-element; Added new scroll element; Added Battery Widget

---
+++ widgets/Battery Widget/__init__.py
@@ -0,0 +1,45 @@
+from cream.contrib.melange import api
+
+import acpi
+
+@api.register('battery')
+class BatteryWidget(api.API):
+
+    def __init__(self, widget):
+
+        self.widget = widget
+
+
+    def get_state(self):
+
+        handle = open('widgets/BatteryWidget/test.txt', 'w')
+        handle.write('bla')
+        handle.close()
+
+        battery_status = acpi.battery_status()
+        battery_remaining_capacity = acpi.battery_remaining_capacity(float)
+        
+        battery_level = 'N/A'
+        
+        if battery_remaining_capacity <= 100.0 and battery_remaining_capacity >= 85.0:
+            battery_level = 'full'
+
+        elif battery_remaining_capacity < 85.0 and battery_remaining_capacity >= 60.0:
+            battery_level = 'full-soon'
+
+        elif battery_remaining_capacity < 60.0 and battery_remaining_capacity >= 40.0:
+            battery_level = 'full-half'
+
+        elif battery_remaining_capacity < 40.0 and battery_remaining_capacity >= 21.0:
+            battery_level = 'empty-soon'
+
+        elif battery_remaining_capacity < 21.0 and battery_remaining_capacity >= 0.0:
+            battery_level = 'empty-critical'
+        
+        battery_remaining_capacity = str(round(battery_remaining_capacity, 2))
+
+        return {
+            'status' : battery_status, 
+            'level' : battery_level,
+            'remaining_capacity' : battery_remaining_capacity
+        }
