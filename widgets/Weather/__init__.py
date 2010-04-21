b965807ac3fa05c9cb1e8e734f93e6a352c9bbdb
Jonas Haag <jonas@lophus.org>
2010-04-21 15:10:06 +0200

    Replaced minidom with lxml and implemented update timeout in WeatherWidget.

--- widgets/Weather/__init__.py
+++ widgets/Weather/__init__.py
@@ -2,29 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import urllib
-from xml.dom.minidom import parseString as parse_xml
+from contextlib import closing
+from lxml.etree import parse as parse_xml
 
 from cream.contrib.melange import api
+
+
+WEATHER_REQUEST_URL = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query={0}'
+KILOMETER_MILES_RATIO = 1.609
+
 
 @api.register('weather')
 class Weather(api.API):
 
     @api.expose
     def get(self, location):
-
-        handle = urllib.urlopen('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query={0}'.format(location))
-        data = handle.read()
-        handle.close()
-
-        dom = parse_xml(data)
+        url = WEATHER_REQUEST_URL.format(location)
+        with closing(urllib.urlopen(url)) as file_handle:
+            weather_data = parse_xml(file_handle)
 
         return {
-            'weather': dom.getElementsByTagName('weather')[0].childNodes[0].data,
-            'temperature': dom.getElementsByTagName('temp_c')[0].childNodes[0].data,
-            'humidity': dom.getElementsByTagName('relative_humidity')[0].childNodes[0].data.replace("%", ""),
-            'wind_direction': dom.getElementsByTagName('wind_dir')[0].childNodes[0].data,
-            'wind_speed': str(round(float(dom.getElementsByTagName('wind_mph')[0].childNodes[0].data) * 1.609, 1)),
-            'pressure': dom.getElementsByTagName('pressure_mb')[0].childNodes[0].data,
-            'visibility': dom.getElementsByTagName('visibility_km')[0].childNodes[0].data,
-            'icon': dom.getElementsByTagName('icon')[0].childNodes[0].data
+            'weather': weather_data.find('weather').text,
+            'temperature': weather_data.find('temp_c').text,
+            'humidity': weather_data.find('relative_humidity').text[:-1], # strip the '%'
+            'wind_direction': weather_data.find('wind_dir').text,
+            'wind_speed': str(round(float(weather_data.find('wind_mph').text) * KILOMETER_MILES_RATIO, 1)),
+            'pressure': weather_data.find('pressure_mb').text,
+            'visibility': weather_data.find('visibility_km').text,
+            'icon': weather_data.find('icon').text
             }
