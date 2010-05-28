d7cc6d2172750bd3d5b35fad5a2ee18c14d1d17d
Friedrich Weber <fred@reichbier.de>
2010-05-28 15:10:11 +0200

    Application Indicators Widget: Popupmenu is working!

--- widgets/Application Indicators/__init__.py
+++ widgets/Application Indicators/__init__.py
@@ -5,52 +5,49 @@ import gobject
 
 from cream.contrib.melange import api
 
+from cream_indicator.host import StatusNotifierHost
+
+def construct_js_item(item):
+    return {
+        'icon': '...',
+        'id': item.id,
+    }
+
 @api.register('appindicators')
 class AppIndicators(api.API):
 
     def __init__(self):
 
-        self.items = []
-
         # Intialize the DBus stuff here...
+        self.host = StatusNotifierHost()
+        self.host.connect('item-added', self.sig_item_added)
+        self.host.connect('item-removed', self.sig_item_removed)
 
+        self.add_initially()
 
-    def add_item(self, item):
+    def sig_item_added(self, host, item):
+        self.emit('item-added', construct_js_item(item))
 
-        # Do some stuff here...
-
-        self.items.append(item)
-
-        self.emit('item-added', {
-            'icon': '...',
-            'id': 'the-items-id'
-            })
-
-
-    def remove_item(self, item):
-
-        # Do some stuff here...
-
-        self.items.remove(item)
+    def sig_item_removed(self, host, item):
         self.emit('item-removed', item.id)
 
+    def add_initially(self):
+        for item in self.host.items:
+            self.sig_item_added(self.host, item)
 
     @api.expose
     def get_items(self):
 
         items = []
 
-        for item in self.items:
-            items.append({
-                'icon': '...',
-                'id': 'the-items-id'
-                })
+        for item in self.host.items:
+            items.append(construct_js_item(item))
 
         return items
 
 
     @api.expose
     def show_menu(self, id):
-
-        # Show the items menu...
-        print id
+        item = self.host.get_item_by_id(id)
+        # Show the menu.
+        item.show_menu()
