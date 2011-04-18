cc89afec3548e407c8d0dd1ab8cc2094204632ba
Kristoffer Kleine <kris.kleine@yahoo.de>
2011-04-18 21:31:45 +0200

    Make spinner size configurable

--- src/data/themes/light/ui/spinner/spinner.js
+++ src/data/themes/light/ui/spinner/spinner.js
@@ -1,13 +1,17 @@
 var Spinner = new Class({
-    initialize: function(elm) {
+    initialize: function(elm, size) {
         this.spokes = 9;
+        if(size)
+            this.size = size
+        else
+            this.size = 30
         this.interval_id = null;
 
         elm.innerHTML = '';
 
         this.canvas = new Element('canvas', {
-            'width': '30',
-            'height': '30',
+            'width': this.size,
+            'height': this.size,
             'style': 'display: inline;'
         });
 
@@ -18,9 +22,9 @@ var Spinner = new Class({
         elm.grab(this.container, 'top');
 
         this.ctx = this.canvas.getContext('2d');
-        this.ctx.translate(15, 15);	// Center the origin
-        this.ctx.lineWidth = 3;
-        this.ctx.lineCap = "round"
+        this.ctx.translate(this.size/2, this.size/2);	// Center the origin
+        this.ctx.lineWidth = this.size/10;
+        this.ctx.lineCap = "round";
     },
     start: function() {
         this.interval_id = this.draw.periodical(100, this);
@@ -29,14 +33,14 @@ var Spinner = new Class({
         this.interval_id = $clear(this.interval_id);
     },
     draw: function() {
-        this.ctx.clearRect(-15, -15, 30, 30);		// Clear the image
+        this.ctx.clearRect(-this.size/2, -this.size/2, this.size, this.size);		// Clear the image
         this.ctx.rotate(Math.PI*2/this.spokes);	// Rotate the origin
         for (var i=0; i<this.spokes; i++) {
             this.ctx.rotate(Math.PI*2/this.spokes);	// Rotate the origin
             this.ctx.strokeStyle = "rgba(0,0,0,"+ i/this.spokes +")";	// Set transparency
             this.ctx.beginPath();
-            this.ctx.moveTo(0, 6);
-            this.ctx.lineTo(0, 14);
+            this.ctx.moveTo(0, this.size/5);
+            this.ctx.lineTo(0, this.size/2.5);
             this.ctx.stroke();
         }
     }
