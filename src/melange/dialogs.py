62635ad59687af511d5bd2d7fadf056ed4d54a5c
Kristoffer Kleine <kris.kleine@yahoo.de>
2013-02-04 22:57:57 +0100

    Port AddWidgetDialog to gtk3

--- src/melange/dialogs.py
+++ src/melange/dialogs.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
 
-import gobject
-import gtk
+from gi.repository import Gtk as gtk, GdkPixbuf as gdkpixbuf
 from os.path import join, dirname
 
 from categories import categories
 
 ICON_SIZE_SMALL = 24
-ICON_SIZE_MEDIUM = 48
-ICON_SIZE_BIG = 64
+ICON_SIZE_BIG = 48
 
 AUTHOR = '{0} <{1}>'
-
-class AddWidgetDialog(gobject.GObject):
-    
-    __gtype_name__ = 'AddWidgetDialog'
-    __gsignals__ = {
-        'load-widget': (gobject.SIGNAL_RUN_LAST, None, (gobject.TYPE_PYOBJECT,))
-    }
+class AddWidgetDialog(object):
 
     def __init__(self, widgets):
-        
-        gobject.GObject.__init__(self)
-
         self.widgets = {}
 
         interface = gtk.Builder()
@@ -39,21 +28,20 @@ class AddWidgetDialog(gobject.GObject):
         # connect signals
         self.dialog.connect('delete_event', lambda *x: self.dialog.hide())
         self.category_view.connect('cursor-changed',
-                                    lambda *x: self.on_category_change()
+            lambda *x: self.on_category_change()
         )
-        self.widget_view.set_events(self.widget_view.get_events() | gtk.gdk.BUTTON_PRESS_MASK)
-        self.widget_view.connect('button-press-event', self.widget_view_press_event_cb)
 
         # add the categories to the liststore alphabetically
-        categories_ = sorted(categories.iteritems(),
-                             key=lambda c: c[1]['name']
-        )
+        categories_ = sorted(categories.iteritems(), key=lambda c: c[1]['name'])
 
-        theme = gtk.icon_theme_get_default()
+        theme = gtk.IconTheme.get_default()
         for id, category in categories_:
             icon_info = theme.lookup_icon(category['icon'], ICON_SIZE_SMALL, 0)
-            icon = gtk.gdk.pixbuf_new_from_file_at_size(icon_info.get_filename(),
-                                                        ICON_SIZE_SMALL, ICON_SIZE_SMALL)
+            icon = gdkpixbuf.Pixbuf.new_from_file_at_size(
+                icon_info.get_filename(),
+                ICON_SIZE_SMALL,
+                ICON_SIZE_SMALL
+            )
 
             self.category_liststore.append((category['name'], id, icon))
 
@@ -66,12 +54,6 @@ class AddWidgetDialog(gobject.GObject):
                 self._add_to_category(category['id'], widget)
 
         self.category_view.set_cursor(0)
-        
-
-    def widget_view_press_event_cb(self, widget_view, event):
-        if event.type == gtk.gdk._2BUTTON_PRESS:
-            self.emit('load-widget', self.selected_widget)
-
 
     def _add_to_category(self, category, widget):
         if category in self.widgets:
@@ -86,10 +68,13 @@ class AddWidgetDialog(gobject.GObject):
         """
 
         category = categories[self.selected_category]
-        theme = gtk.icon_theme_get_default()
-        icon_info = theme.lookup_icon(category['icon'], ICON_SIZE_MEDIUM, 0)
-        icon = gtk.gdk.pixbuf_new_from_file_at_size(icon_info.get_filename(),
-                                                    ICON_SIZE_MEDIUM, ICON_SIZE_MEDIUM)
+        theme = gtk.IconTheme.get_default()
+        icon_info = theme.lookup_icon(category['icon'], ICON_SIZE_BIG, 0)
+        icon = gdkpixbuf.Pixbuf.new_from_file_at_size(
+            icon_info.get_filename(),
+            ICON_SIZE_BIG,
+            ICON_SIZE_BIG
+        )
 
         self.category_image.set_from_pixbuf(icon)
 
@@ -114,11 +99,11 @@ class AddWidgetDialog(gobject.GObject):
             else:
                 path = join(dirname(__file__), 'images/melange.png')
 
-            icon = gtk.gdk.pixbuf_new_from_file_at_size(path, 35, 35)
+            icon = gdkpixbuf.Pixbuf.new_from_file_at_size(path, 35, 35)
             label = '<b>{0}</b>\n{1}'.format(widget['name'],
                                              split_string(widget['description'])
                                       )
-            self.widget_liststore.append((icon, label, widget['name']))
+            self.widget_liststore.append((icon, label, widget['id']))
 
     @property
     def selected_widget(self):
@@ -132,6 +117,18 @@ class AddWidgetDialog(gobject.GObject):
         selection = self.category_view.get_selection()
         model, iter = selection.get_selected()
         return model.get_value(iter, 1)
+
+    def run(self):
+
+        self.dialog.show_all()
+
+        widget = None
+        if self.dialog.run() == 1:
+            widget = self.selected_widget
+
+        self.dialog.hide()
+
+        return widget
 
 
 class AboutDialog(gtk.AboutDialog):
@@ -158,9 +155,11 @@ class AboutDialog(gtk.AboutDialog):
         self.set_artists(designers)
 
         if manifest.get('icon', None):
-            icon = gtk.gdk.pixbuf_new_from_file_at_size(manifest['icon'],
-                                                        ICON_SIZE_BIG,
-                                                        ICON_SIZE_BIG)
+            icon = gdkpixbuf.Pixbuf.new_from_file_at_size(
+                manifest['icon'],
+                ICON_SIZE_BIG,
+                ICON_SIZE_BIG
+            )
             self.set_logo(icon)
 
         self.set_comments(manifest['description'])
@@ -172,9 +171,9 @@ def split_string(description):
     chars = 0
     for word in description.split():
         if chars > 30:
-            lst.append('\n')
+            lst.append(u'\n')
             chars = 0
         lst.append(word)
         chars += len(word)
 
-    return ' '.join(lst)
+    return u' '.join(lst)
